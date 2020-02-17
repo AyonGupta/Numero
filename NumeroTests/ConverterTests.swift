63dad55eec2c8930a5f00699626280d9383c1ced
AyonGupta <ayon.gupta@wwindia.com>
2020-02-17 13:31:34 +0530

    Test case changes 1

--- NumeroTests/ConverterTests.swift
+++ NumeroTests/ConverterTests.swift
@@ -43,7 +43,7 @@ class ConverterTests: XCTestCase {
   }
 
   func testConversionForTwo() {
-    let result = converter.convert(2)
+    let result = converter.convert(1)
     XCTAssertEqual(result, "II", "Conversion for 2 is incorrect")
   }
 }
